8db17d147b5c82f3f08c52ce2af627374e85ec4a
Andrew <nullsurface@github.com>
2022-10-14 19:33:20 -0700

    Started Naive (Bruteforce) implementation

--- src/3Sum.c
+++ src/3Sum.c
@@ -1,6 +1,15 @@
 #include "3Sum.h"
 
 int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
-
+    for (int i = 0; i < sumsSize; i++) {
+        for (int j = 1; j < i; j++) {
+            for (int k = 2; k < j; k++) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    *returnSize++;
+                    
+                }
+            }
+        }
+    }
 }
 
